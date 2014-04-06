96af64530ea51177db97e5a617b796b1d3296ede
Robert Yang <robertyi@126.com>
2014-04-06 10:20:17 +0800

    v1.2 update include("dbc.php")

--- student_mark_change.php
+++ student_mark_change.php
@@ -1,19 +1,5 @@
 <?php
-session_start();
-$mysql_servername = "localhost"; //主机地址
-$mysql_username = "root"; //数据库用户名
-$mysql_password =""; //数据库密码
-$mysql_database ="student"; //数据库
-mysql_connect($mysql_servername , $mysql_username , $mysql_password);
-mysql_select_db($mysql_database); 
-mysql_query("set character set 'utf8'");//读库
-mysql_query("set names 'utf8'");//写库
-
-if(mysqli_connect_errno())
-{
-echo "连接数据库失败";
-exit;
-}  
+include("dbc.php");
 ?>
 
 <!DOCTYPE html>
